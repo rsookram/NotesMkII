aa190476b6bae5e2c3651a81b555580d29160625
rsookram <rashadsookram@gmail.com>
2020-01-05 20:20:16 -0500

    Create empty ViewModel

--- app/src/main/java/io/github/rsookram/notesmkii/MainActivity.kt
+++ app/src/main/java/io/github/rsookram/notesmkii/MainActivity.kt
@@ -1,10 +1,23 @@
 package io.github.rsookram.notesmkii
 
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toolbar
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelLazy
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : FragmentActivity(R.layout.activity_main) {
+
+    @Suppress("UNCHECKED_CAST")
+    private val vm by ViewModelLazy(MainViewModel::class, { this.viewModelStore }) {
+        object : ViewModelProvider.Factory {
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T =
+                MainViewModel() as T
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,13 +28,23 @@ class MainActivity : FragmentActivity(R.layout.activity_main) {
         toolbar.setOnMenuItemClickListener {
             when (it.itemId) {
                 R.id.open -> {
+                    vm.onOpenClicked()
                     true
                 }
                 R.id.create -> {
+                    vm.onCreateClicked()
                     true
                 }
                 else -> false
             }
         }
+
+        val editor = findViewById<TextView>(R.id.text)
+        editor.doOnTextChanged { text, _, _, _ -> vm.onTextChanged(text.toString()) }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        vm.save()
     }
 }
---
+++ app/src/main/java/io/github/rsookram/notesmkii/MainViewModel.kt
@@ -0,0 +1,22 @@
+package io.github.rsookram.notesmkii
+
+import android.net.Uri
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+    fun onOpenClicked() {
+    }
+
+    fun onCreateClicked() {
+    }
+
+    fun onUriSelected(uri: Uri) {
+    }
+
+    fun onTextChanged(text: String) {
+    }
+
+    fun save() {
+    }
+}
