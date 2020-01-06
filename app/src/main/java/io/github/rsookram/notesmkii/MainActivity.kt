852aa76d65b8883431782c778297b91fd4cc20a8
rsookram <rashadsookram@gmail.com>
2020-01-05 20:07:47 -0500

    Add Toolbar menu

--- app/src/main/java/io/github/rsookram/notesmkii/MainActivity.kt
+++ app/src/main/java/io/github/rsookram/notesmkii/MainActivity.kt
@@ -1,12 +1,27 @@
 package io.github.rsookram.notesmkii
 
 import android.os.Bundle
+import android.widget.Toolbar
 import androidx.fragment.app.FragmentActivity
 
-class MainActivity : FragmentActivity() {
+class MainActivity : FragmentActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        toolbar.inflateMenu(R.menu.toolbar)
+
+        toolbar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.open -> {
+                    true
+                }
+                R.id.create -> {
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
